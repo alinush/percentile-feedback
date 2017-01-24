25ffec20ec7798e29ec7fc5c9100e4c253195305
Alin Tomescu <alinush@mit.edu>
2017-01-24 11:26:41 -0500

    changed port

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -3,7 +3,7 @@ sudo apt-get install nginx
 
 # Read how to setup nginx here: https://nginx.org/en/docs/beginners_guide.html
 sudo cp nginx/percentile /etc/nginx/sites-available/
-sudo rm /etc/nginx/sites-enabled/default
+sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/percentile /etc/nginx/sites-enabled/
 
 sudo service nginx restart
